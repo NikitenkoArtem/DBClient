0e9ee6abbc0a7664e1ed7aa0e4db04d92cb459ca
git <ghost-hero2012@mail.ru>
2016-05-06 01:13:12 +0300

    Add show methods, cut close() from Front End, execute statements

--- src/oraclient/sql/conns/DBConnection.java
+++ src/oraclient/sql/conns/DBConnection.java
@@ -6,6 +6,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.SQLWarning;
 import java.sql.Statement;
 
 import java.util.Arrays;
@@ -28,30 +29,26 @@ import oraclient.view.FrontEndForm;
 
 public class DBConnection {
     private static Connection connection;
-    private static Statement stmt;
-//    private static String url = "jdbc:oracle:thin:@localhost:1521:xe";
-//    private static String username = "hr"; //"scott";
-//    private static String password = "hr"; //"tiger";
-    private static String url;
-    private static String username;
-    private static String password;
+//    private static Statement stmt;
+    private static String url = "jdbc:oracle:thin:@localhost:1521:xe";
+    private static String username = "hr";
+    private static String password = "hr";
+    //    private static String url;
+    //    private static String username;
+    //    private static String password;
     private Map<Object, Object> result = new HashMap<>();
     private boolean connected = false;
 
     public static Connection getConnection() throws SQLException {
-                ConnectionDialog dialog = new ConnectionDialog();
-                dialog.setVisible(true);
-                url = dialog.getUrl().getText();
-                username = dialog.getUsrname().getText();
-                password = dialog.getPwd().getText();
+        //                ConnectionDialog dialog = new ConnectionDialog();
+        //                dialog.setVisible(true);
+        //                url = dialog.getUrl().getText();
+        //                username = dialog.getUsrname().getText();
+        //                password = dialog.getPwd().getText();
         //            JOptionPane.showMessageDialog(getClass(), e);
         //        DriverManager.registerDriver(new OracleDriver());
         connection = DriverManager.getConnection(url, username, password);
         //        conn.setAutoCommit(false);
-        return connection;
-    }
-
-    public Connection getConn() {
         return connection;
     }
 
@@ -60,32 +57,41 @@ public class DBConnection {
                       conn.getMetaData().getUserName());
     }
 
-    public void exec(Connection conn, JTextArea sqlFile) throws SQLException {
-        stmt = conn.createStatement();
-        stmt.execute(sqlFile.getText());
+    public Statement exec(Connection conn, JComponent text) throws SQLException {
+//        stmt = conn.createStatement();
+//        stmt.execute(sqlFile.getText());
+        Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        //            stmt.executeQuery("select * from departments");
+        stmt.executeUpdate("drop table test");
+        stmt.execute("create table test(id int, name varchar2(5), price int)");
+        stmt.executeUpdate("insert into test values(1, 'hello', 12345)");
+        stmt.executeQuery("select * from test");
+        //            stmt.execute(area.getTextAreas().iterator().next().getText());
+        return stmt;
     }
 
-    public void getResultSet(Statement stat, JComponent console, JComponent table) {
+    public void getResultSet(Statement stmt, JComponent console, JComponent table) {
         try {
-            DatabaseMetaData dbMeta = connection.getMetaData();
-            ResultSet rs = stat.getResultSet();
+            //            getDBMetaData();
+            ResultSet rs = stmt.getResultSet();
             ResultSetMetaData meta = rs.getMetaData();
             DefaultTableModel tableModel = new DefaultTableModel(0, 0);
+            getColumnLabels(console, meta);
             while (rs.next()) {
                 for (int i = 1; i <= meta.getColumnCount(); i++) {
-                    result.put(meta.getColumnLabel(i), rs.getObject(i));                    
+                    result.put(meta.getColumnLabel(i), rs.getObject(i));
                 }
-                
-                System.out.println(result.values().toString());
                 if (tableModel.getColumnCount() == 0) {
                     addTableColumn(tableModel, result);
                 }
-                addTableRow(tableModel);                
-                getQueryResult(console, table);
+                addTableRow(tableModel);
+                println(console);
             }
-            
             ((JTable) table).setModel(tableModel);
-        } catch (SQLException e) {
+            showWarnings(console, stmt);
+            ((JTextArea) console).append("\n");
+        } catch (SQLException e) {            
+            showExceptions(console, e);
         }
     }
 
@@ -100,26 +106,61 @@ public class DBConnection {
         tableModel.addRow(result.values().toArray());
     }
 
-    private void getColumnLabels() {
-        Object[] keys = new Object[result.size()];
-        int index = 0;
-        for (Map.Entry<Object, Object> entry : result.entrySet()) {
-            keys[index] = entry.getKey();
-            index++;
+    private void getColumnLabels(JComponent console, ResultSetMetaData meta) throws SQLException {
+        for (int i = 1; i <= meta.getColumnCount(); i++) {
+            ((JTextArea) console).append(meta.getColumnLabel(i) + "  ");
         }
-//        return keys;
+        //        ((JTextArea) console).append(Arrays.toString(result.keySet().toArray()));
+        ((JTextArea) console).append("\n");
     }
 
-    private void getQueryResult(JComponent console, JComponent table) {
-//        if (((JTextArea) console).getText() != "") {
-//            ((JTextArea) console).append(Arrays.toString(result.keySet().toArray()) + "\n");
-//        }
+    private void println(JComponent console) {
         ((JTextArea) console).append(Arrays.toString(result.values().toArray()) + "\n");
-//        Object[] values = result.values().toArray();
-//        for (Object value : values) {
-//            if (value != null) {
-//                ((JTextArea) console).append(value.toString() + "\t");        
-//            }
-//        }
+    }
+
+    private void showWarnings(JComponent console, Statement stat) throws SQLException {
+        SQLWarning warning = stat.getWarnings();
+        while (warning != null) {
+            ((JTextArea) console).append(warning.getCause() + "\n");
+            warning.getNextWarning();
+        }
+    }
+
+    private void showExceptions(JComponent console, SQLException ex) {
+        Iterator<Throwable> it = ex.iterator();
+        while (it.hasNext()) {
+            ((JTextArea) console).append(ex.getSQLState() + "\n");
+            ((JTextArea) console).append(ex.getErrorCode() + "\n");
+        }
+    }
+
+    private void getDBMetaData() throws SQLException {
+        DatabaseMetaData dbMeta = connection.getMetaData();
+    }
+    
+    public void close() {
+        try {
+        } finally {
+            if(connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                }
+    //                    connected = false;
+            }
+        
+    //            try {
+    //                if(stmt != null)
+    //                    stmt.close();
+    //            } catch (Exception e) {
+    //                e.printStackTrace();
+    //            }
+    //            try {
+    //                if(rs != null)
+    //                    rs.close();
+    //            } catch (Exception e) {
+    //                e.printStackTrace();
+    //            }
+        }
     }
 }
