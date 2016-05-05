ff8e7ab64f5e4985cf953d004ffa1b8201be6790
git <ghost-hero2012@mail.ru>
2016-05-06 01:03:15 +0300

    Extract OutputStream from constructor

--- src/oraclient/io/NewFile.java
+++ src/oraclient/io/NewFile.java
@@ -15,37 +15,31 @@ import javax.swing.JTabbedPane;
 import oraclient.view.FileJDialog;
 
 public class NewFile extends File {
-    private List<File> file;
+    private List<File> files;
     private String currentfile;
     private boolean saved = false;
 
     public NewFile(String filePath) {
         super(filePath);
-        if (file == null) {
+        if (files == null) {
             init();
         }
         this.addFile(new File(filePath));
-        ClientOutputStream out;
-        try {
-            out = new ClientOutputStream();
-            out.addOutputStream(new FileOutputStream(filePath));
-        } catch (FileNotFoundException ex) {
-        }
     }
     
     private void init() {
-        file = new ArrayList<>();
+        files = new ArrayList<>();
     }
 
     public List<File> getFile() {
-        return file;
+        return files;
     }
 
-    public void addFile(File f) {
-        file.add(f);
+    public void addFile(File file) {
+        files.add(file);
     }
     
     public void close() {
-        file.clear();
+        files.clear();
     }
 }
