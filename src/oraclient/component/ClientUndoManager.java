95c02391637f2cacc3c9584b39db321ac486c247
git <ghost-hero2012@mail.ru>
2016-05-07 00:47:53 +0300

    List changed on Map, find method added

--- src/oraclient/component/ClientUndoManager.java
+++ src/oraclient/component/ClientUndoManager.java
@@ -4,7 +4,10 @@ import java.io.File;
 import java.io.OutputStream;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
+import java.util.Map;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -14,7 +17,8 @@ import javax.swing.undo.UndoManager;
 import oraclient.io.NewFile;
 
 public class ClientUndoManager {
-    private List<UndoManager> undoMgrs;
+    private Map<JTextArea, UndoManager> undoMgrs;
+//    private List<UndoManager> undoMgrs;
 
     public ClientUndoManager() {
         if (undoMgrs == null) {
@@ -23,16 +27,35 @@ public class ClientUndoManager {
     }
 
     private void init() {
-        undoMgrs = new ArrayList<>();
+        undoMgrs = new HashMap<>();
+//        undoMgrs = new ArrayList<>();
     }
-    
-    public List<UndoManager> getUndoMgrs() {
+
+    public Map<JTextArea, UndoManager> getUndoMgrs() {
         return undoMgrs;
     }
 
-    public void addUndoManager(UndoManager mgr) {
-        undoMgrs.add(mgr);
+    public void addUndoManager(JTextArea area, UndoManager mgr) {
+        undoMgrs.put(area, mgr);
     }
+
+    public UndoManager find(JTextArea key) {
+        System.out.println(key);
+        for (Map.Entry<JTextArea, UndoManager> entry : undoMgrs.entrySet()) {
+            if (key == entry.getKey()) {
+                return entry.getValue();
+            }
+        }
+        return null;
+    }
+    
+//    public List<UndoManager> getUndoMgrs() {
+//        return undoMgrs;
+//    }
+//
+//    public void addUndoManager(UndoManager mgr) {
+//        undoMgrs.add(mgr);
+//    }
     
     public void close() {
         undoMgrs.clear();
