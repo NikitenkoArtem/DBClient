c026c780d0207477bc94b2b450de581af470a4e5
git <ghost-hero2012@mail.ru>
2016-05-30 00:18:30 +0200

    Map<File, String> with dependent methods deleted

--- src/dbclient/io/NewFile.java
+++ src/dbclient/io/NewFile.java
@@ -17,8 +17,7 @@ import javax.swing.JTextArea;
 import dbclient.swing.ClientArea;
 
 public class NewFile extends File {
-    private Map<File, String> files;
-    private Map<File, Boolean> saved;
+    private Map<File, Boolean> files;
 
     public NewFile(String filePath) {
         super(filePath);
@@ -29,53 +28,43 @@ public class NewFile extends File {
 
     private void init() {
         files = new HashMap<>();
-        saved = new HashMap<>();
     }
 
-    public Map<File, String> getFiles() {
+    public Map<File, Boolean> getFiles() {
         return files;
     }
 
-    public Map<File, Boolean> getSaved() {
-        return saved;
-    }
-
-    public void putFile(File file, String filePath) {
-        files.put(file, filePath);
-    }
-
-    public void putSave(File key, Boolean value) {
-        saved.put(key, value);
+    public void putFile(File key, Boolean value) {
+        files.put(key, value);
     }
 
     public boolean isSaved(File key) {
-        return saved.get(key).booleanValue();
+        return files.get(key).booleanValue();
     }
 
     public boolean isSavedAll() {
-        if (saved.containsValue(false)) {
+        if (files.containsValue(false)) {
             return false;
         } else {
             return true;
         }
     }
 
-    public void save(File file, JComponent area) {
-        if (area != null) {
+    public void save(File file, JComponent textArea) {
+        if (textArea != null) {
             try (OutputStream out = new FileOutputStream(file)) {
-                out.write(((JTextArea) area).getText().getBytes());
+                out.write(((JTextArea) textArea).getText().getBytes());
             } catch (IOException e) {
             }
         }
     }
 
-    public void saveAs(File file, JComponent area) {
-        if (area != null) {
+    public void saveAs(File file, JComponent textArea) {
+        if (textArea != null) {
             try (OutputStream out = new FileOutputStream(file)) {
                 try {
-                    out.write(((JTextArea) area).getText().getBytes());
-                    putFile(file, file.getAbsolutePath());
-                    putSave(file, true);
+                    out.write(((JTextArea) textArea).getText().getBytes());
+                    putFile(file, true);
                 } catch (FileNotFoundException e) {
                 }
             } catch (IOException e) {
@@ -83,11 +72,11 @@ public class NewFile extends File {
         }
     }
 
-    public void saveAll(ClientArea area) {
-        if (area != null) {
-            for (Map.Entry<File, String> entry : files.entrySet()) {
+    public void saveAll(ClientArea textArea) {
+        if (textArea != null) {
+            for (Map.Entry<File, Boolean> entry : files.entrySet()) {
                 try (OutputStream out = new FileOutputStream(entry.getKey())) {
-                    out.write(area.find(entry.getKey()).getText().getBytes());
+                    out.write(textArea.find(entry.getKey()).getText().getBytes());
 //                    saved.replace(entry.getKey(), isSaved(entry.getValue()), true);
                 } catch (FileNotFoundException e) {
                 } catch (IOException e) {
@@ -97,7 +86,7 @@ public class NewFile extends File {
     }
 
     public File find(File key) {
-        for (Map.Entry<File, String> entry : files.entrySet()) {
+        for (Map.Entry<File, Boolean> entry : files.entrySet()) {
             if (key == entry.getKey()) {
                 return entry.getKey();
             }
@@ -105,8 +94,18 @@ public class NewFile extends File {
         return null;
     }
 
-    public File find(String value) {
-        for (Map.Entry<File, String> entry : files.entrySet()) {
+    public File find(String absoluteFilePath) {
+        for (Map.Entry<File, Boolean> entry : files.entrySet()) {
+            String absolutePath = entry.getKey().getAbsolutePath();
+            if (absoluteFilePath.equalsIgnoreCase(absolutePath)) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+
+    public File find(Boolean value) {
+        for (Map.Entry<File, Boolean> entry : files.entrySet()) {
 //            if (value == entry.getValue()) {
             if (value.equals(entry.getValue())) {
                 return entry.getKey();
@@ -117,8 +116,7 @@ public class NewFile extends File {
 
     public void openFile(File file, JComponent component) {
         try {
-            files.put(file, file.getAbsolutePath());
-            saved.put(file, false);
+            files.put(file, false);
             try (InputStream in = new FileInputStream(file)) {
                 int i = 0;
                 do {
